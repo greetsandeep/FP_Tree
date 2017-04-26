50063e668bf3dce3a470ca18557db57112b3bd2c
snehalw7 <snehal.wadhwani7@gmail.com>
2017-04-26 19:15:33 +0530

    Tree traversal of given dataset

--- FP_Tree/FP_TREE/src/tree_fp/FP_Growth.java
+++ FP_Tree/FP_TREE/src/tree_fp/FP_Growth.java
@@ -6,8 +6,9 @@ public class FP_Growth {
 	public static ArrayList<int []> data = new ArrayList<int[]>();
 	public static void main(String args[]){
 		PreProcessing pre = new PreProcessing("rule.data");
-		int[] row = new int[9];
+		int[] row;
 		for(int i=0;i<pre.expandedData.size();i++){
+			row = new int[9];
 			for(int j=0;j<pre.expandedData.get(i).length;j++){
 				row[j] = pre.expandedData.get(i)[j];
 			}
@@ -15,15 +16,21 @@ public class FP_Growth {
 		}
 		
 		FP_Tree fp = new FP_Tree(-1,null,0);
-		int test1[] = {1,3,4};
+		/*int test1[] = {1,3,4};
 		int test2[] = {1,2,5};
 		int test3[] = {1,2,6};
 		fp.addTransaction(test1);
 		fp.addTransaction(test2);
 		fp.addTransaction(test3);
+		fp.treeTraversal();*/
+		
+		for(int i=0;i<data.size();i++){
+			/*for(int j=0;j<data.get(i).length;j++){
+				System.out.print(data.get(i)[j]+" ");
+			}
+			System.out.println();*/
+			fp.addTransaction(data.get(i));
+		}
 		fp.treeTraversal();
-		/*for(int i=0;i<data.size();i++){
-			
-		}*/
 	}
 }
--- FP_Tree/FP_TREE/src/tree_fp/FP_Tree.java
+++ FP_Tree/FP_TREE/src/tree_fp/FP_Tree.java
@@ -2,13 +2,31 @@ package tree_fp;
 
 import java.util.*;
 
+/**
+ * @author Poojitha, Snehal, Sandeep
+ *	Class that implements the node of the FP Tree.
+ */
 public class FP_Tree {
+	/** The no. of transactions this element is present at this position */
 	int count;
+	
+	/** The item represented by the node */
 	int item;
+	
+	/** The position int the tree at which this node is present */
 	ArrayList<Integer> pos;
+	
+	/** Children of this node */
 	ArrayList<FP_Tree> children;
+	
+	/** Reference pointer to the next node of the same item */
 	FP_Tree ref;
 	
+	/**
+	 * @param item The item which this node represents
+	 * @param parentPos The position of the parent in the tree
+	 * @param pos The child number of this node 
+	 */
 	FP_Tree(int item, ArrayList<Integer> parentPos, int pos){
 		this.item = item;
 		this.count = 1;
@@ -23,11 +41,19 @@ public class FP_Tree {
 		}
 	}
 	
+	
+	/**
+	 * @param item The item to be inserted in the children list of this node
+	 */
 	public void addChild(int item){
 		FP_Tree child = new FP_Tree(item,this.pos,this.children.size()+1);
 		this.children.add(child);
 	}
 	
+	
+	/**
+	 * @param transaction The transaction to be added in the tree. Takes transaction as an array of integers.
+	 */
 	public void addTransaction(int transaction[]){
 		FP_Tree node = this;
 		boolean flag = false;
@@ -47,6 +73,9 @@ public class FP_Tree {
 		}
 	}
 	
+	/**
+	 * Traverses the tree rooted at this node in a depth-first fashion and prints the item, its count and it's position in the tree.
+	 */
 	public void treeTraversal(){
 		ArrayList<FP_Tree> stack = new ArrayList<FP_Tree>();
 		FP_Tree node = this;
@@ -55,7 +84,6 @@ public class FP_Tree {
 			node = stack.get(0);
 			for(int i=node.children.size()-1;i>=0;i--){
 				stack.add(0,node.children.get(i));
-					System.out.println(i);
 			}
 			if(node.item!=-1){
 				System.out.print("(");
@@ -68,7 +96,7 @@ public class FP_Tree {
 				System.out.print(node.item+":");
 				System.out.println(node.count);
 			}
-			stack.remove(0);
+			stack.remove(node);
 		}
 	}
 }
